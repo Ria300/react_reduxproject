c1c4662e681e77f87924b6d612498c0f20c79675
Ria300 <riaguptakalka@gmail.com>
2019-11-27 09:15:34 +0530

    project

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,43 @@
-import React from 'react'
-const Contact=()=>{
-    return (
-     <div className="container">
-         <h4 className="center"> Contact</h4>
-        <p>ria</p>
-     </div>
-    )
+import React,{Component} from 'react'
+class Contact extends Component{
+   constructor(props){
+       super(props);
+       this.state={
+           items:[],
+           isLoaded:false,
+       }
+   }
+   componentDidMount(){
+       fetch('https://jsonplaceholder.typicode.com/users')
+       .then(res=>res.json())
+       .then(json=>{
+           this.setState({
+               isLoaded:true,
+               items:json,
+           })
+       })
+   }
+    render(){
+        var{isLoaded,items}=this.state;
+        if(!isLoaded)
+        {
+            return <div>Loading</div>
+        }
+        else{
+        return(
+<div>
+    <ul>
+
+        {items.map(items=>(
+<li key={items.id}>
+  Name: {items.name}
+  Email:{items.email} 
+</li>
+        ))}
+    </ul>
+</div>
+        )
+    }
+}
 }
 export default Contact;
